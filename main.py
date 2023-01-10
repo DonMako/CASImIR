e9389fcd9891538860c3c781af7985f22dae90e8
Don Mako <lucas0203@orange.fr>
2023-01-10 15:29:50 +0100

    Création du squelette du code

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import urllib
 from urllib import request
-from urllib import robotparser
-
-# On initialise le nombre d'urls trouvés, car une des conditions d'arrêt est 50 urls trouvés.
-nb_urls_found = 0
-url = "https://ensai.fr/"
+from bs4 import BeautifulSoup as BS
 
 
-ensai = urllib.request.urlopen(url)
-robot = urllib.robotparser.RobotFileParser(url)
+def main():
 
-print(robot)
+    # On stocke les urls des pages déjà croisées, pour ne pas stocker 
+    liste_urls = []
+
+    # On initialise l'objet url, qui va nous permettre de faire la recherche
+    url = "https://ensai.fr/"
+    with open('crawled_webpages.txt', 'w') as f:
+        f.write(url)
+
+    while len(liste_urls) < 50:
+
+        url_request = urllib.request.urlopen(url)
+        soup = BS(url_request, 'html.parser')
+        urls_found = []
+        for link in soup.find_all('a'):
+            urls_found.append(link.get('href'))
+
+        for link in urls_found:
+            if link not in liste_urls:
+                liste_urls.append(link)
+    
+    for link in liste_urls:
+        with open('crawled_webpages.txt', 'w') as f:
+            f.write(link)
+
+main()
