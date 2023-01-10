5fbd9abaa4c01030febd63aaf8cd333dfb3f91c7
Don Mako <lucas0203@orange.fr>
2023-01-10 16:33:11 +0100

    Tentative de prendre en compte les URL non conformes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import urllib
-from urllib import request
+from urllib import *
 from bs4 import BeautifulSoup as BS
 
 def main(url, threshold):
@@ -27,18 +27,24 @@ def main(url, threshold):
 
 
 
-
+# La fonction permettant de trouver d'autres pages à explorer en analysant les balises de liens trouvées 
 def requeter(liste_urls, index):
 
     urls_found = []
+    
+    # On crée un try/except afin de ne pas crawler un site web qui l’interdit.
     try:
         url_request = urllib.request.urlopen(liste_urls[index])
         soup = BS(url_request, 'html.parser')
     
+        # Pour chaque lien de page trouvé, on vérifie que le lien est un URL valide.
+        # Si le lien est valide, on l'ajoute dans les liens trouvés.
         for link in soup.find_all('a'):
-            if link != "#header":
-                urls_found.append(link.get('href'))
-
+            #parsed_url = urllib.parse.urlparse.urlparse(link)
+            #if bool(parsed_url.scheme):
+            #    urls_found.append(link.get('href'))
+            urls_found.append(link.get('href'))
+            
     except:
         pass
 
@@ -47,4 +53,4 @@ def requeter(liste_urls, index):
 
 
 
-main("https://ensai.fr/", 200)
+main("https://ensai.fr/", 15)
