48562f8231790f0b001a6ae065cbd8fecfeb3cc6
Don Mako <lucas0203@orange.fr>
2023-01-13 17:47:44 +0100

    Finalisation de la fonction de consultation des robots.txt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from urllib import robotparser
 from bs4 import BeautifulSoup as BS
 
 
+
 # La fonction principale, permettant de boucler les recherches sur les URL, afin d'atteindre le seuil d'URL demandé par l'utilisateur.
 def main(url, threshold):
 
@@ -36,18 +37,17 @@ def requeter(liste_urls, index):
 
     urls_found = []
     
-    # On crée un try/except afin de ne pas crawler un site web qui l’interdit.
     try:
-        url_request = request.urlopen(liste_urls[index])
+        url_crawled = liste_urls[index]
+        url_request = request.urlopen(url_crawled)
         soup = BS(url_request, 'html.parser')
     
-        # Pour chaque lien de page trouvé, on vérifie que le lien est un URL valide.
+        # Pour chaque lien de page trouvé, on vérifie que le lien est un URL autorisé à être crawlé.
         # Si le lien est valide, on l'ajoute dans les liens trouvés.
         for link in soup.find_all('a'):
-            #parsed_url = parse.urlparse.urlparse(link.get('href'))
-            #if bool(parsed_url.scheme):
-            #    urls_found.append(link.get('href'))
-            urls_found.append(link.get('href'))
+            ref = link.get('href')
+            if ref in analyse_robot(url_crawled):
+                urls_found.append()
 
     except:
         pass
@@ -56,18 +56,28 @@ def requeter(liste_urls, index):
 
 
 
-# La fonction permettant d'analyser le fichier robot.txt d'une page, afin de déterminer si les liens qu'elle contient sont autorisés à crawler.
+# La fonction permettant d'analyser le fichier robot.txt d'une page, afin de récupérer les liens qu'elle contient qui peuvent être crawlés.
 def analyse_robot(url):
 
     parser = robotparser.RobotFileParser()
     parser.set_url(parse.urljoin(url, 'robots.txt'))
+    
+    # On initialise la liste des url qui peuvent être crawlés
+    url_allowed = []
+
+    # On fait un try/except au cas où la page web n'a pas de robots.txt
     try:
         parser.read()
+        for line in parser:
+            if line.startswith('Allow'):
+                url_allowed.append(line.split(': ')[1].split(' ')[0]) 
     except:
         pass
+    
+    return(url_allowed)
 
 
-analyse_robot("https://ensai.fr/")
-#url_user = input("Enter the starting url : ")
-#threshold_user = input("Enter the threshold desired : ")
-#main(url_user, threshold_user)
+
+url_user = input("Enter the starting url : ")
+threshold_user = input("Enter the threshold desired : ")
+main(url_user, threshold_user)
