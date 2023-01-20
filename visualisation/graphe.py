ac217ca8ea0808cd9cbe22857de22e5608dc9a27
Don Mako <lucas0203@orange.fr>
2023-01-20 19:46:39 +0100

    Correctif du fichier des graphes orientés

--- visualisation/graphe.py
+++ visualisation/graphe.py
@@ -1,8 +1,3 @@
-import io
-import base64
-
-
-
 def add_edge(graphe, link_depart, link_arrive):
 
     if graphe.has_edge(link_depart, link_arrive):
@@ -17,13 +12,4 @@ def add_node(graphe, link):
 
     if graphe.has_node(link):
         graphe.add_node(link)
-
-
-def fig_to_base64(fig):
-
-    img = io.BytesIO()
-    fig.savefig(img, format='png', bbox_inches='tight')
-    img.seek(0)
-
-    return base64.b64encode(img.getvalue())
 
