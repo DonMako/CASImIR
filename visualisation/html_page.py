2306d8c1f1784f9a80520b093087325326384171
Don Mako <lucas0203@orange.fr>
2023-01-20 20:08:48 +0100

    Préparation des fichiers pour le lancement de la page web

--- visualisation/html_page.py
+++ visualisation/html_page.py
@@ -1,8 +1,15 @@
 from visualisation.graphe import fig_to_base64
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def generate_page(graphe):
 
     encoded = fig_to_base64(graphe)
     my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
+
+
+
+#def display_html():
+
+    
