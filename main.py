94b1c90c5911592223b9c9f36625a97a8efcb152
Don Mako <lucas0203@orange.fr>
2023-01-24 15:03:45 +0100

    Réécriture de la fonction main()

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from crawler.robots import request_robots
-from graphe.graphe import add_edge, add_node
-from graphe.token import tokenise_list, tokenise
+from crawler.crawler import crawl
 from visualisation.table import generate_table
 import networkx as nx
 
@@ -17,19 +15,7 @@ def crawler():
     starting_index = 0
     G = nx.Graph()
 
-    while len(list_urls) < int(threshold) and starting_index < len(list_urls):
-
-        url_request = list_urls[starting_index]
-        result = request_robots(url_request)
-        tokens_result = tokenise_list(result)
-        tokens_request = tokenise(url_request)
-        for list_token in tokens_result:
-            for token in list_token:
-                add_node(G, token)
-                for token_init in tokens_request:
-                    add_edge(G, token_init, token)
-
-        starting_index += 1
+    crawl(list_urls, starting_index, threshold, G)
         
     f = open("crawled_webpages.txt", "w")
     for link in list_urls[:int(threshold)]:
