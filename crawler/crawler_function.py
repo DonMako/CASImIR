f864b55ca3223f189cb2e358044ac656442c059d
Don Mako <lucas0203@orange.fr>
2023-02-07 12:16:01 +0100

    Modification des pycache

--- crawler/crawler_function.py
+++ crawler/crawler_function.py
@@ -1,6 +1,5 @@
-import robots
-import graphe.graphe as graphe
-import graphe.token as token_function
+from robots import *
+from graphe import graphe_function, token_function
 
 
 def crawl(list_urls, starting_index, threshold, graphique):
@@ -8,13 +7,13 @@ def crawl(list_urls, starting_index, threshold, graphique):
     while len(list_urls) < int(threshold) and starting_index < len(list_urls):
 
         url_request = list_urls[starting_index]
-        result = robots.request_robots(url_request)
+        result = request_robots(url_request)
         tokens_result = token_function.tokenise_list(result)
         tokens_request = token_function.tokenise(url_request)
         for list_token in tokens_result:
             for token in list_token:
-                graphe.add_node(graphique, token)
+                graphe_function.add_node(graphique, token)
                 for token_init in tokens_request:
-                    graphe.add_edge(graphique, token_init, token)
+                    graphe_function.add_edge(graphique, token_init, token)
 
         starting_index += 1
