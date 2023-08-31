0b53e515dd0d710ea7e4dec9abc659121f371b4f
Lucas Macaux <lucas0203@orange.fr>
2023-08-31 19:10:18 +0200

    Retour au travauk

--- visualisation/graphe_function.py
+++ visualisation/graphe_function.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-
 def set_edge(graphe: nx.Graph, depart: str, arrive: str):
 
     if graphe.has_edge(depart, arrive):
@@ -12,18 +11,14 @@ def set_edge(graphe: nx.Graph, depart: str, arrive: str):
         graphe.add_edge(depart, arrive)
 
 
-
 def get_node_size(token: str):
 
     return(len(token))
 
 
-
 def set_node(graphe: nx.Graph, token: str):
-
     if not(graphe.has_node(token)):
-        graphe.add_node(token, size = get_node_size(token))
-
+        graphe.add_node(token, size=get_node_size(token))
 
 
 def create_graphe(graphe: nx.Graph):
@@ -32,7 +27,9 @@ def create_graphe(graphe: nx.Graph):
     for node in graphe.nodes():
         labels[node] = node
     pos = nx.spring_layout(graphe)
-    nx.draw_networkx(graphe, pos, node_shape = "s", node_color = "yellow", with_labels = False)
+    nx.draw_networkx(graphe, pos, node_shape="s",
+                     node_color="yellow", with_labels=False)
     nx.draw_networkx_labels(graphe, pos, labels)
     plt.tight_layout()
-    plt.savefig("./visualisation/graphe.png", bbox_inches = "tight", pad_inches = 0, format = "png")
+    plt.savefig("./visualisation/graphe.png",
+                bbox_inches="tight", pad_inches=0, format="png")
