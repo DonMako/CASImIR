840c832ab814e86750cd7df6074180b32f7a7080
DonMako <80451849+DonMako@users.noreply.github.com>
2025-02-27 22:27:21 +0100

    Init des params de la classe User

--- objets/User.py
+++ objets/User.py
@@ -1,1 +1,11 @@
-class User
+class User:
+    """
+    :param str id: Identifiant of the user
+    :param str platform: Where the user posted the images
+    :param list images: List of images the user posted
+    """
+
+    def __init__(self, id: str, platform: str, images: list):
+        self.id = id
+        self.platform = platform
+        self.images = images
