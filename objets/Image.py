b033240a6a98c773af4d39874842dff9f2dab5de
DonMako <80451849+DonMako@users.noreply.github.com>
2025-02-27 22:20:11 +0100

    Init des params de la classe Image

--- objets/Image.py
+++ objets/Image.py
@@ -1,1 +1,11 @@
-class Image
+class Image():
+    """
+    :param str url: URL of the image (where it was originally found)
+    :param str name: Name under which the image is locally stored
+    :param int score: Score of confident the model has for the image to represent a child
+    """
+
+    def __init__(self, url: str, score: int):
+        self.url = url
+        self.name = self.url.split('/')[-1]
+        self.score = score
